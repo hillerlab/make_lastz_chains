2c57f8f5fa26cdaeef6f0efdf8aea219746bfbfd
Bogdan Kirilenko <kirilenkobm@gmail.com>
2023-09-10 23:26:19 +0200

    probably a bit improved partition part

--- steps_implementations/partition.py
+++ steps_implementations/partition.py
@@ -12,6 +12,42 @@ def do_partition_for_genome(genome_label, project_dir, params, executables):
     seq_len_file = params.seq_1_len if genome_label == Constants.TARGET_LABEL else params.seq_2_len
     chunk_size = params.seq_1_chunk if genome_label == Constants.TARGET_LABEL else params.seq_2_chunk
     overlap = params.seq_1_lap if genome_label == Constants.TARGET_LABEL else params.seq_2_lap
+    # seq_limit = 0  # Replace with your actual seq limit if any
+
+    # Read chromosome sizes
+    chrom_sizes = read_chrom_sizes(seq_len_file)
+
+    # Create output directories
+    output_dir = os.path.join(project_dir, Constants.TEMP_LASTZ_DIRNAME)
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Initialize partition list and small chrom list
+    partition_list = []
+    partition_file_path = os.path.join(output_dir, f"{genome_label}_partitions.txt")
+
+    for chrom, size in chrom_sizes.items():
+        start = 0
+        while start < size:
+            end = min(start + chunk_size, size)
+            partition = f"{os.path.abspath(seq_dir)}:{chrom}:{start}-{end}"
+            partition_list.append(partition)
+            start += chunk_size - overlap  # Move the start point for the next chunk
+
+    # Save the partition list to a file
+    with open(partition_file_path, 'w') as f:
+        for part in partition_list:
+            f.write(f"{part}\n")
+
+
+# Not necessary: just a reference, translated from perl
+def reference_old_do_partition_for_genome(genome_label, project_dir, params, executables):
+    print(f"# Partitioning for {genome_label}")
+
+    # Determine directories and filenames based on the genome label
+    seq_dir = params.seq_1_dir if genome_label == Constants.TARGET_LABEL else params.seq_2_dir
+    seq_len_file = params.seq_1_len if genome_label == Constants.TARGET_LABEL else params.seq_2_len
+    chunk_size = params.seq_1_chunk if genome_label == Constants.TARGET_LABEL else params.seq_2_chunk
+    overlap = params.seq_1_lap if genome_label == Constants.TARGET_LABEL else params.seq_2_lap
     seq_limit = 0  # Replace with your actual seq limit if any
 
     # Read chromosome sizes
